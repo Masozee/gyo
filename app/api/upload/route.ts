a8385c0f0474b2bb5c5c38bad8f0e6b75f51cab6
Masozee <nurojilukmansyah@gmail.com>
2025-06-25 16:05:52 +0700

    change to edge

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 import { writeFile, mkdir } from 'fs/promises';
 import { join } from 'path';
 import { existsSync } from 'fs';
